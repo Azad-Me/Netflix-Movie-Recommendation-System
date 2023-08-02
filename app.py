8643499bf91085df5c1941a952426142529551bd
Azad Anil Meshram <122529968+Azad-Me@users.noreply.github.com>
2023-08-02 18:03:41 +0530

    app.py

--- app.py
+++ app.py
@@ -10,28 +10,6 @@ data = data1.iloc[:2500,:]
 with bz2.BZ2File('final_compressed_data.pkl', 'rb') as f:
     similarities = pickle.load(f)
 
-def set_bg_hack_url():
-    '''
-    A function to unpack an image from url and set as bg.
-    Returns
-    -------
-    The background.
-    '''
-        
-    st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background: url("https://pixabay.com/get/g3ad6aae6a5c46e43ea6ee422570620bf9e1762384056a34d0c4d95303ef347068d3800318ebd1fc0e80f85025ed4eb63.jpg");
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
-
-set_bg_hack_url()
-
 def movies_recommendation(movie_nme):
 
   print('Hey!! Gyus, Checkout these Amazing Movies')
@@ -69,8 +47,8 @@ def poster_file(movie_name):
     return poster
 
 
-#st.title(':white[Netflix Movies Recommendation System]')
-st.markdown("<h1 style='color: white;'>Netflix Movies Recommendation System</h1>", unsafe_allow_html=True)
+st.title(':white[Netflix Movies Recommendation System]')
+#st.markdown("<h1 style='color: white;'>Netflix Movies Recommendation System</h1>", unsafe_allow_html=True)
 option = st.selectbox(
         'Movie List',
         data['title'].unique())
